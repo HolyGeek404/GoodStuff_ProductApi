48a961afa3719b4306ff8f172697308b77b7864d
HolyGeek404 <wiktorzme@gmail.com>
2025-06-12 14:04:34 +0200

    Added Product logic

---
+++ Model/DataAccess/ProductDao.cs
@@ -0,0 +1,26 @@
+using Microsoft.Azure.Cosmos;
+namespace Model.DataAccess;
+
+public class ProductDao(CosmosClient cosmosClient) : IProductDao
+{
+    public async Task<object?> GetAllProductsByType(string type)
+    {
+        var container = cosmosClient.GetContainer("GoodStuff", "Products");
+        var query = new QueryDefinition("SELECT * FROM c WHERE c.Category = @category")
+            .WithParameter("@category", type);
+
+        var queryOptions = new QueryRequestOptions
+        {
+            PartitionKey = new PartitionKey(type)
+        };
+
+        using var iterator = container.GetItemQueryIterator<Dictionary<string, object>>(query, requestOptions: queryOptions);
+        var response = await iterator.ReadNextAsync();
+
+        if (response.Count == 0)
+        {
+            return null;
+        }
+        return response;
+    }
+}
