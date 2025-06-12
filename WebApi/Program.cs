fb72b3e1cc551a1ca736926fda52cd94b895971d
HolyGeek404 <wiktorzme@gmail.com>
2025-06-12 14:47:58 +0200

    Middleware setup

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,41 +1,29 @@
+using WebApi.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddServices();
+builder.Services.AddMediatRConfig();
+builder.Services.AddAzureConfig(builder.Configuration);
+builder.Services.AddDataBaseConfig(builder.Configuration);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerConfig(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.MapOpenApi();
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "GoodStuff WebApi v1");
+    c.OAuthClientId(builder.Configuration["Swagger:SwaggerClientId"]);
+    c.OAuthUsePkce();
+    c.OAuthScopeSeparator(" ");
 }
-
+    );
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
