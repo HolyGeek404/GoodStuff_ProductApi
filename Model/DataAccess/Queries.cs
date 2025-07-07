ee4c1658388fb7d5c8e4f0e0e07127f91b5d362e
HolyGeek404 <wmatkowski404@proton.me>
2025-07-07 17:09:27 +0200

    Refactor SQL queries in Queries.cs for better readability and maintainability

--- Model/DataAccess/Queries.cs
+++ Model/DataAccess/Queries.cs
@@ -3,18 +3,28 @@ namespace Model.DataAccess;
 public static class Queries
 {
     #region Basic
-    private static readonly string BasicGetAllColumns = "c.Category, c.ProductId, c.ProductImg, c.Name, c.Price,";
-    private static readonly string BasicGetAllByCategoryFrom = "FROM c WHERE c.Category = @category";
-    private static readonly string BasicGetSingleById = $"SELECT * {BasicGetAllByCategoryFrom} AND c.ProductId = @id";
+    private static readonly string BasicColumns = "c.Category, c.ProductId, c.ProductImg, c.Name, c.Price,";
+    private static readonly string BasicCategoryFrom = "FROM c WHERE c.Category = @category";
+    private static readonly string BasicCategoryFromWithProductId = $"{BasicCategoryFrom} AND c.ProductId = @id";
     #endregion  
 
-    private static readonly string GetAllGpus = $"SELECT {BasicGetAllColumns} c.RecommendedPSUPower, c.MemoryBus, c.CoreRatio {BasicGetAllByCategoryFrom}";
-    private static readonly string GetAllCpus = $"SELECT {BasicGetAllColumns} c.Socket, c.Architecture, c.TDP {BasicGetAllByCategoryFrom}";
-    private static readonly string GetAllCoolers = $"SELECT {BasicGetAllColumns} c.Fans, c.RPMControll, c.Compatibility, c.HeatPipes {BasicGetAllByCategoryFrom}";
+    #region Gpu
+    private static readonly string GetAllGpusExtension = "c.RecommendedPSUPower, c.MemoryBus, c.CoreRatio";
+    private static readonly string GetAllGpus = $"SELECT {BasicColumns} {GetAllGpusExtension} {BasicCategoryFrom}";
+    private static readonly string GetSingleGpu = $"SELECT {BasicColumns} {GetAllGpusExtension} c.GpuProcessorLine, c.PCIeCategory, c.MemorySize, c.MemoryCategory, c.MemoryRatio, c.CoresNumber, c.CoolingCategory, c.OutputsCategory, c.SupportedLibraries, c.PowerConnector, c.Length, c.Width, c.Height, c.Warranty, c.ProducentCode, c.PgpCode, c.GpuProcessorName, c.Manufacturer, c.ProductImg {BasicCategoryFromWithProductId}";
+    #endregion
 
-    private static readonly string GetSingleCpu = $"SELECT c.Name, c.Team, c.Price, c.Familiy, c.Series, c.Socket, c.SupportedChipsets, c.RecomendedChipset, c.Architecture, c.Frequency, c.Cores, c.Threads, c.UnlockedMultipler, c.CacheMemory, c.IntegredGpu, c.IntegredGpuModel, c.SupportedRam, c.Litography, c.TDP, c.AdditionalInfo, c.IncludedCooler, c.Warranty, c.ProductImg, c.Category, c.ProductId  {BasicGetAllByCategoryFrom} AND c.ProductId = @id";
-    private static readonly string GetSingleGpu = $"SELECT c.ProductId, c.Name, c.Price, c.Team, c.GpuProcessorLine, c.PCIeCategory, c.MemorySize, c.MemoryCategory, c.MemoryBus, c.MemoryRatio, c.CoreRatio, c.CoresNumber, c.CoolingCategory, c.OutputsCategory, c.SupportedLibraries, c.PowerConnector, c.RecommendedPSUPower, c.Length, c.Width, c.Height, c.Warranty, c.ProducentCode, c.PgpCode, c.GpuProcessorName, c.Manufacturer, c.ProductImg, c.Category {BasicGetAllByCategoryFrom} AND c.ProductId = @id";
-    private static readonly string GetSingleCooler = $"SELECT c.Name, c.Team, c.CoolerType, c.Compatibility, c.Size, c.HeatPipes, c.Fans, c.RPMControll, c.RMP, c.BearingType, c.FanSize, c.Connector, c.SupplyVoltage, c.SupplyCurrent, c.Highlight, c.MTBFLifetime, c.Height, c.Width, c.Depth, c.Weight, c.Warranty, c.ProducentCode, c.ProductImg, c.Price, c.Manufacture, c.Category, c.ProductId  {BasicGetAllByCategoryFrom} AND c.ProductId = @id";
+    #region Cpu
+    private static readonly string GetAllCpusExtension = "c.Socket, c.Architecture, c.TDP";
+    private static readonly string GetAllCpus = $"SELECT {BasicColumns} {GetAllCpusExtension} {BasicCategoryFrom}";
+    private static readonly string GetSingleCpu = $"SELECT {BasicColumns} {GetAllCpusExtension}, c.Familiy, c.Series, c.SupportedChipsets, c.RecomendedChipset, c.Frequency, c.Cores, c.Threads, c.UnlockedMultipler, c.CacheMemory, c.IntegredGpu, c.IntegredGpuModel, c.SupportedRam, c.Litography, c.AdditionalInfo, c.IncludedCooler, c.Warranty {BasicCategoryFromWithProductId}";
+    #endregion
+
+    #region Cooler
+    private static readonly string GetAllCoolersExtension = "c.Fans, c.RPMControll, c.Compatibility, c.HeatPipes";
+    private static readonly string GetAllCoolers = $"SELECT {BasicColumns} {GetAllCoolersExtension} {BasicCategoryFrom}";
+    private static readonly string GetSingleCooler = $"SELECT {BasicColumns} {GetAllCoolersExtension}, c.CoolerType, c.Compatibility, c.Size, c.HeatPipes, c.Fans, c.RPMControll, c.RMP, c.BearingType, c.FanSize, c.Connector, c.SupplyVoltage, c.SupplyCurrent, c.Highlight, c.MTBFLifetime, c.Height, c.Width, c.Depth, c.Weight, c.Warranty, c.ProducentCode, c.Manufacture, {BasicCategoryFromWithProductId}";
+    #endregion
 
     public static string GetAllByType(string type)
     {
