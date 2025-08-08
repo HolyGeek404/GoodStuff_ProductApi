69262d9a3ac01a70555388235570e1b60ad90cd7
HolyGeek404 <wmatkowski404@proton.me>
2025-08-08 11:34:50 +0200

    Changed return type of BaseProductDao.cs funcs to T type

--- Model/DataAccess/BaseProductDao.cs
+++ Model/DataAccess/BaseProductDao.cs
@@ -3,7 +3,7 @@ namespace Model.DataAccess;
 
 public abstract class BaseProductDao(CosmosClient cosmosClient)
 {
-    protected async Task<object?> Get<T>(QueryDefinition queryDefinition, string type)
+    protected async Task<T?> Get<T>(QueryDefinition queryDefinition, string type)
     {
         var container = cosmosClient.GetContainer("GoodStuff", "Products");
         var queryRequestOptions = new QueryRequestOptions
@@ -13,9 +13,9 @@ public abstract class BaseProductDao(CosmosClient cosmosClient)
         using var iterator = container.GetItemQueryIterator<T>(queryDefinition, requestOptions: queryRequestOptions);
         var response = await iterator.ReadNextAsync();  
 
-        return response.Count == 0 ? null : response.Resource;
+        return response.Count == 0 ? default : response.Resource.FirstOrDefault();
     }
-    protected async Task<object?> GetList<T>(QueryDefinition queryDefinition, string type)
+    protected async Task<List<T>?> GetList<T>(QueryDefinition queryDefinition, string type)
     {
         var container = cosmosClient.GetContainer("GoodStuff", "Products");
         var queryRequestOptions = new QueryRequestOptions
