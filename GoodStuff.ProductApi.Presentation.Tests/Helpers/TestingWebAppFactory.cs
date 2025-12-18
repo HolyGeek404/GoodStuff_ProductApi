3999832c7db460f75b91964c8cfcba48f34d691c
HolyGeek404 <wmatkowski404@proton.me>
2025-12-18 17:46:10 +0100

    Added integration tests for GET endpoints

---
+++ GoodStuff.ProductApi.Presentation.Tests/Helpers/TestingWebAppFactory.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace GoodStuff.ProductApi.Presentation.Tests.Helpers;
+
+public class TestingWebAppFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = "Test";
+                    options.DefaultChallengeScheme = "Test";
+                })
+                .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>("Test", options => { });
+
+
+            builder.Configure(app =>
+            {
+                app.UseAuthentication();
+                app.UseAuthorization();
+            });
+        });
+    }
+}
